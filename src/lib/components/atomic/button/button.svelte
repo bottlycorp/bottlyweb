c68172bb963e39e4c677f129a2c3b80ac56ed5d0
Gaëtan H <steellgold0@gmail.com>
2023-05-06 15:05:57 +0200

    feat(button): add Button component with support for different colors and types
    The Button component is a new addition to the project. It is a reusable atomic component that can be used throughout the application. It supports different colors such as primary, secondary, danger, gold, and transparent. It also supports different types such as button, submit, and reset. The component is implemented in Svelte and uses the clsx library for generating class names.

---
+++ src/lib/components/atomic/button/button.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import type { ButtonColor, ButtonType } from "./button.types";
+  import clsx from "clsx";
+
+  export let type: ButtonType = "button";
+  export let color: ButtonColor = "transparent";
+  export let disabled: boolean = false;
+  export let onClick: () => void = () => {};
+
+  $: style = clsx("flex gap-3 items-center rounded-[5px] px-4 pt-1 pb-1 text-sm font-medium border-none", {
+    "bg-primary": color === "primary",
+    "bg-primary cursor-not-allowed": color === "primary" && disabled,
+    "bg-secondary": color === "secondary",
+    "bg-secondary cursor-not-allowed": color === "secondary" && disabled,
+    "bg-danger": color === "danger",
+    "bg-danger cursor-not-allowed": color === "danger" && disabled,
+    "bg-gold": color === "gold",
+    "bg-gold cursor-not-allowed": color === "gold" && disabled,
+    "bg-transparent text-black": color === "transparent",
+    "bg-transparent cursor-not-allowed text-black": color === "transparent" && disabled,
+    "text-white": color !== "transparent",
+    "text-black": color === "transparent"
+  });
+</script>
+
+<button
+  class="{style}"
+  type={type}
+  disabled={disabled}
+  on:click={onClick}
+>
+  <slot />
+</button>
---
+++ src/lib/components/atomic/button/button.types.ts
@@ -0,0 +1,2 @@
+export type ButtonType = 'button' | 'submit' | 'reset';
+export type ButtonColor = "primary" | "secondary" | "danger" | "gold" | "transparent";
